02b98dfa1ca0d7f27e828cead5fc6ed4f5441255
Vadym Maslovskyi <improvemylife25@gmail.com>
2021-06-12 15:34:13 +0300

    'prod'

--- src/js/main.js
+++ src/js/main.js
@@ -6,11 +6,19 @@ import arrow from './modules/arrow';
 import './basic/dynamic_adaptive';
 import './modules/setLocalization';
 import './modules/localization';
+import timer from './modules/timer';
+import raise from './modules/raisebet';
+import checkMail from './modules/checkemail';
 
+const deadline = '2021-06-15',
+      timers = document.querySelectorAll('.time__count');
 
 
 window.addEventListener('DOMContentLoaded', () => {
   headerH('.header', '.header__container');
   burger('.header__burger', '.header__menu-body');
   arrow();
+  timer(timers, deadline);
+  raise('.lots__button', 5000);
+  checkMail('.forms__mail-input', '.forms__mail-btn');
 });
